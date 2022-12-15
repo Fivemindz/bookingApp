8d537abd40ab3ddc881a535f5b805ec8cfbb93f2
Fivemindz <JD>
2022-12-14 19:08:58 -0600

    created base file structure with correct dependencies in package.json-deployed to aws successfully

---
+++ src/lambda/http/getBookings.ts
@@ -0,0 +1,17 @@
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Event: ', event)  
+  const response = 'Hello from the booking app'
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      message: response
+    }) 
+  }
+}
